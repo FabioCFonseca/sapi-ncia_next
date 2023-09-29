eb94cd4d46f8050a847a607184c959e353e8193e
Fabio <fc_102@hotmail.com>
2023-09-28 22:58:42 -0300

    First commit, pages and navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <Link href="/">Home</Link>
+      <h1>Navbar</h1>
+    </div>
+  )
+}
+
+export default Navbar
